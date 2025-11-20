100423b3c17ed4df9d2979bc56fc9c251700fb5b
Hendrik Bruinsma <hbruinsma@xs4some.nl>
2025-11-20 14:18:39 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cp -r .gitconfig ~/.gitconfig
+cp -r .vimrc ~/.vimrc
+cp -r .bashrc ~/.bashrc
+cp -r .bash_profile ~/.bash_profile
+cp -r .oh-my-bash ~/.oh-my-bash
+cp -r .editorconfig ~/.editorconfig
